943869a521f1d36d37fbea74d2c7cd6e269cf98a
Lemon Hall <lemonhall@gmail.com>
2025-02-03 01:53:10 +0800

    增加了阿里百炼的demo

---
+++ bailian_deepseek_demo.py
@@ -0,0 +1,33 @@
+import os
+from openai import OpenAI
+
+# -----------------------------------------------------------------------------
+# 使用 PowerShell
+# 打开 PowerShell（在 “开始” 菜单中搜索 “PowerShell” 并打开）。
+# 要为当前用户设置环境变量，可以使用
+# $env:SILICONFLOW_API_KEY = "your_api_key"
+# 命令。
+# 同样，将"your_api_key"替换为实际的 API 密钥。不过，这种方式设置的环境变量只在当前 PowerShell 会话中有效。
+
+# 要永久设置环境变量（对于当前用户），可以使用
+# [Environment]::SetEnvironmentVariable("DASHSCOPE_API_KEY","your_api_key","User")。
+# 如果要设置系统级别的环境变量（需要管理员权限），可以将最后一个参数改为"Machine"，
+# 例如
+# [Environment]::SetEnvironmentVariable("DASHSCOPE_API_KEY","your_api_key","Machine")。
+# Set up SILICONFLOW API key
+# 记得使用以上方法后，需要关闭vscode后重启vscode，之后点击F5运行python脚本的时候才能生效
+
+client = OpenAI(
+    # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
+    api_key=os.getenv("DASHSCOPE_API_KEY"), # 如何获取API Key：https://help.aliyun.com/zh/model-studio/developer-reference/get-api-key
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+completion = client.chat.completions.create(
+    model="deepseek-v3", 
+    messages=[
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': '你是谁？'}
+        ]
+)
+print(completion.choices[0].message.content)
